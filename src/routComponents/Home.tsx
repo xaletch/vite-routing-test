0db2f4d534314f1d63ead7c9467a6a843868aa8a
Xaletch <kiril.kolesnikov0609@mail.ru>
2024-05-25 13:06:48 +0300

    test 2

--- src/routComponents/Home.tsx
+++ src/routComponents/Home.tsx
@@ -1,3 +1,23 @@
+import styles from "./style.module.css";
+
 export const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className={styles.home}>
+      <h2 className="page__title">Главная</h2>
+      <div className={styles.home__grid}>
+        <div className={styles.home__grid_card}>1</div>
+        <div className={styles.home__grid_card}>2</div>
+        <div className={styles.home__grid_card}>3</div>
+        <div className={styles.home__grid_card}>4</div>
+        <div className={styles.home__grid_card}>5</div>
+        <div className={styles.home__grid_card}>6</div>
+      </div>
+      {/*  */}
+      <div className={styles.home__bottom_grid}>
+        <div className={styles.home__grid_card}>8</div>
+        <div className={styles.home__grid_card}>9</div>
+        <div className={styles.home__grid_card}>10</div>
+      </div>
+    </div>
+  );
 };
